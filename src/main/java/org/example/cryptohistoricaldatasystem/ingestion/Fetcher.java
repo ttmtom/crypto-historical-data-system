6a54cf16aaa710fecdc7d80a88f72b30d499ec4d
Tom Tam <tmtam921@gamil.com>
2025-08-11 18:19:17 +0800

    Revert "ingestion init"
    This reverts commit 8e27feb4cab009ae69d2f29c0e15482ff732758c.

--- src/main/java/org/example/cryptohistoricaldatasystem/ingestion/Fetcher.java
+++
@@ -1,8 +0,0 @@
-package org.example.cryptohistoricaldatasystem.ingestion;
-
-import org.example.cryptohistoricaldatasystem.ingestion.dto.Kline;
-import reactor.core.publisher.Flux;
-
-public interface Fetcher {
-    Flux<Kline> fetch();
-}
