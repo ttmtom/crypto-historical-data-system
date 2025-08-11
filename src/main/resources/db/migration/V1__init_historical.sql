32b7dc9a3aac56d8c0e49260f6b18f6b3ab0a5b9
Tom Tam <tmtam921@gamil.com>
2025-08-12 01:37:57 +0800

    sql typo

--- src/main/resources/db/migration/V1__init_historical.sql
+++ src/main/resources/db/migration/V1__init_historical.sql
@@ -12,9 +12,10 @@ CREATE TABLE historical_records
     close_price     DECIMAL                     NOT NULL,
     volume          DECIMAL                     NOT NULL,
     source_data     JSONB                       NOT NULL,
-    CONSTRAINT pk_historical_records PRIMARY KEY (timestamp, source, symbol)
+    CONSTRAINT pk_historical_records PRIMARY KEY (timestamp, source, symbol, close_timestamp)
 );
 
-SELECT create_hypertable('historical_records', by_range('timestamp'), if_not_exists => TRUE);
+SELECT create_hypertable('historical_records', by_range('close_timestamp'), if_not_exists => TRUE);
 
 CREATE INDEX ON historical_records (source, symbol);
+CREATE INDEX ON historical_records (source, symbol, close_timestamp);
